f29fa199753bec01e169cc2cb7c27cb640447057
dasithDevapriya <36620033+is1104@users.noreply.github.com>
2019-12-08 15:19:11 +0530

    signing with ui

--- lib/screens/authentication/signin.dart
+++ lib/screens/authentication/signin.dart
@@ -61,64 +61,136 @@ class _SignInState extends State<SignIn> {
         ),
       ),*/
       //Form
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
+      body: SingleChildScrollView(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: Colors.white,
+          child: Stack(
             children: <Widget>[
-              SizedBox(
-                height: 20,
+              Container(
                 width: double.infinity,
+                height: MediaQuery.of(context).size.height / 2,
+                color: Colors.blue,
               ),
-              TextFormField(
-                validator: (value) => value.isEmpty ? 'Enter an email' : null,
-                onChanged: (value) {
-                  setState(() {
-                    email = value;
-                  });
-                },
-              ),
-              SizedBox(
-                height: 20,
-                width: double.infinity,
-              ),
-              TextFormField(
-                validator: (value) => value.length < 8
-                    ? 'Password must be atleast 8 characters'
-                    : null,
-                obscureText: true,
-                onChanged: (value) {
-                  setState(() {
-                    password = value;
-                  });
-                },
-              ),
-              SizedBox(
-                height: 20,
-                width: double.infinity,
-              ),
-              RaisedButton(
-                color: Colors.blue[900],
-                child: Text('LOGIN', style: TextStyle(color: Colors.white)),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    dynamic result =
-                        await _auth.signInWithEmailandPassword(email, password);
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 50, 50, 125),
+                child: Material(
+                  elevation: 10,
+                  color: Colors.white,
+                  child: Center(
+                    child: Form(
+                      key: _formKey,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                        child: Column(
+                          children: <Widget>[
+                            SizedBox(
+                              height: 25,
+                              width: double.infinity,
+                            ),
+                            Material(
+                              elevation: 10,
+                              child: Container(
+                                height: 50,
+                                width: 50,
+                                decoration: new BoxDecoration(
+                                  image: new DecorationImage(
+                                    image: new AssetImage(
+                                        "assets/images/Logo.png"),
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                hintText: 'email',
+                                fillColor: Colors.grey,
+                                filled: true,
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.grey, width: 2.0)),
+                                focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.blue, width: 2.0)),
+                              ),
+                              validator: (value) =>
+                                  value.isEmpty ? 'Enter an email' : null,
+                              onChanged: (value) {
+                                setState(() {
+                                  email = value;
+                                });
+                              },
+                            ),
+                            SizedBox(
+                              height: 20,
+                              width: double.infinity,
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                hintText: 'password',
+                                fillColor: Colors.grey,
+                                filled: true,
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.grey, width: 2.0)),
+                                focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.blue, width: 2.0)),
+                              ),
+                              validator: (value) => value.length < 8
+                                  ? 'Password must be atleast 8 characters'
+                                  : null,
+                              obscureText: true,
+                              onChanged: (value) {
+                                setState(() {
+                                  password = value;
+                                });
+                              },
+                            ),
+                            SizedBox(
+                              height: 50,
+                              width: double.infinity,
+                            ),
+                            Container(
+                              alignment: Alignment.bottomRight,
+                              child: RaisedButton(
+                                color: Colors.blue[900],
+                                child: Text('LOGIN',
+                                    style: TextStyle(color: Colors.white)),
+                                onPressed: () async {
+                                  if (_formKey.currentState.validate()) {
+                                    dynamic result =
+                                        await _auth.signInWithEmailandPassword(
+                                            email, password);
 
-                    if (result == null) {
-                      setState(() => error = 'COULD NOT SIGNIN');
-                    }
-                  }
-                },
+                                    if (result == null) {
+                                      setState(
+                                          () => error = 'COULD NOT SIGNIN');
+                                    }
+                                  }
+                                },
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              error,
+                              style:
+                                  TextStyle(color: Colors.red, fontSize: 12.0),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 12.0),
-              )
             ],
           ),
         ),
